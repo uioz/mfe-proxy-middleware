f90cb0457ab14b4c0e8ba116fa93b4670f63765a
ASCll <hzxbpld@163.com>
2021-04-25 18:20:59 +0800

    feat: 静态资源机制完善 stage 1 实现

--- src/common.ts
+++ src/common.ts
@@ -1,4 +1,4 @@
-import { mfeConfig, mfeRoute } from 'type';
+import { appProxyOption, mfeConfig, mfeRoute } from 'type';
 
 export const MFE_CONFIG_FILE_NAME = 'mfe-config.js';
 
@@ -13,9 +13,18 @@ export const ROUTE_PATH = `./${MFE_ROUTE_FILE_NAME}`;
 export const STATIC_DIR = './dist/static';
 
 export const DEFAULT_MFE_CONFIG: Required<mfeConfig> = {
-  appThatneededProxy: [],
+  appThatNeededProxy: [],
   outputDir: OUTPUT_DIR,
   routePath: ROUTE_PATH,
-  staticDir: STATIC_DIR,
-  usePackageNameAsStaticPrefix: true,
+  static: {
+    outputDir: STATIC_DIR,
+    publicPath: '/static',
+    staticPrefix: '',
+  },
+};
+
+export const DEFAULT_APP_PROXY_OPTION: Required<appProxyOption> = {
+  publicPath: '/static',
+  route: `/${MFE_ROUTE_FILE_NAME}`,
+  url: '',
 };
